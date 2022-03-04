8bdd5566355655848a4b27e6bea96b253308b597
Poul <79355496+pushok666@users.noreply.github.com>
2022-03-04 16:30:42 +0300

    core api

---
+++ PostService/PostService/Migrations/20220215195220_InitialMigrations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PostService.Migrations
+{
+    public partial class InitialMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Mails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Subject = table.Column<string>(type: "text", nullable: false),
+                    Body = table.Column<string>(type: "text", nullable: true),
+                    Recipients = table.Column<List<string>>(type: "text[]", nullable: false),
+                    SentTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Result = table.Column<string>(type: "text", nullable: false),
+                    FailedMessage = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mails", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Mails");
+        }
+    }
+}
